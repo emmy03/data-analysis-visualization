a1b336d0aa715cf4c26e7b0f7b60026c8922d1dc
Emmy <mjemmy03@gmail.com>
2025-03-08 18:32:39 +0100

    Improve print statements for clarity in ex_3.py

--- ex_3.py
+++ ex_3.py
@@ -10,21 +10,21 @@ except BaseException as erreur:
 
 # Convertir 'plot_id' en flottant
 surveys_df['plot_id'] = surveys_df['plot_id'].astype('float')
-print(surveys_df['plot_id'].dtype)
+print("\nType de la colonne 'plot_id' : ", surveys_df['plot_id'].dtype)
 
 # Déterminer les valeurs non finies, renvoie un masque booléen
-print(surveys_df.isna())
+print("\nMasque des valeurs : \n", surveys_df.isna())
 
 # Déterminer les lignes contenant au moins une valeur non finie
-masque_nan = surveys_df.isna().any(axis='columns').head()
-print(masque_nan)
+masque_nan = surveys_df.isna().any(axis='columns')
+print("\nLignes contenant au moins une valeur non finie : \n", masque_nan)
 
 # Afficher le nombre d'enregistrements par 'species_id' pour les lignes où la colonne 'weight' contient des valeurs non finies
 une_selection = surveys_df[surveys_df['weight'].isna()]
-print(une_selection.groupby('species_id')['record_id'].count())
+print("\nNombre de poids non définit par espèce : \n", une_selection.groupby('species_id')['record_id'].count())
 
 # Afficher le nombre d'observation de la colonne 'weight' et sa moyenne 
-print(surveys_df['weight'].count(), surveys_df['weight'].mean())
+print("\nNombre de poids définit : ", surveys_df['weight'].count(), "\n\nMoyenne des poids : ", surveys_df['weight'].mean())
 
 # Créer une copie pour ne pas modifier l'objet original
 copie_surveys = surveys_df.copy()
@@ -34,10 +34,10 @@ copie_surveys['weight'] = copie_surveys['weight'].fillna(
     copie_surveys['weight'].mean())
 
 # Même moyenne mais toutes les valeurs sont finies
-print(copie_surveys['weight'].count(), copie_surveys['weight'].mean())
+print("\nNombre de poids définit : ", copie_surveys['weight'].count(), "\n\nMoyenne des poids : ", copie_surveys['weight'].mean())
 
 # Puisqu'il n'y a plus de valeurs non finies, on peut convertir 'weight' en entier
 copie_surveys['weight'] = copie_surveys['weight'].astype('int64')
-print(copie_surveys['weight'])
-print("Moyenne : ", copie_surveys['weight'].mean())
+print("\nTableau des poids : \n", copie_surveys['weight'])
+print("\nMoyenne des poids : ", copie_surveys['weight'].mean())
 
